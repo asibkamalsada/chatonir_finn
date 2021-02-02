dcc5aa365ebbef2e9b4736a130359588a7b7181c
spidispiderwick <chrissi.penndorf@gmail.com>
2021-02-02 14:49:31 +0100

    main für asib

--- search/main.py
+++ search/main.py
@@ -1,6 +1,5 @@
-import time
-
 import searchengine
+import time
 
 
 def main():
@@ -9,12 +8,12 @@ def main():
     # se.createIndexAndIndexDocs("dump_no_bodytext.json")
     t2 = time.time()
     print(t2-t1)
-    se.extract_json("test", "test.json")
+    se.create_index()
     se.index_data(se.readJSON("test.json"))
-    # se.update_abstracts("abstracts.json")
+    se.update_abstracts("abstracts.json")
     t3 = time.time()
     print(t3-t2)
-    # se.update_keyqueries()
+    se.update_keyqueries()
     t4 = time.time()
     print(t4-t3)
     # se.print_kqs()
